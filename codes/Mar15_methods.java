c7d7c4b3aadac523eae4fc512a3ddb5198ecca3a
real-lake050317 <jinojoa317@naver.com>
2022-03-15 10:21:23 +0900

    .

---
+++ codes/Mar15_methods.java
@@ -0,0 +1,44 @@
+public class Mar15_methods{
+    public static int sum(int n){
+        if (n == 1)
+            return 1;
+    
+        return n + sum(n - 1);
+    }
+    
+    public static int fibonacci(int n){
+        if (n == 1 || n == 2)
+            return 1;
+    
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    
+    public static int factorial(int n){
+        if (n == 1)
+            return 1;
+    
+        return n * factorial(n - 1);
+    }    
+
+    public static int gcd(int a, int b){
+        if (b == 0)
+            return a;
+    
+        return gcd(b, a % b);
+    }
+
+    void recurMe1(int num){
+       System.out.print(num+" ");
+       if (num<4)
+           recurMe1(num+1);
+    } // 1 2 3 4
+    
+    void recurMe2(int num){
+       if (num<4)
+           recurMe2(num+1);
+       System.out.print(num+" ");
+    } // 4 3 2 1
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
