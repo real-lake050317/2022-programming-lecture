7d0d711a7278048ef042d29f5901024bf5d1189a
real-lake050317 <jinojoa317@naver.com>
2022-03-17 02:55:35 +0900

    .

---
+++ codes/fibonacci.java
@@ -0,0 +1,22 @@
+public class fibonacci{
+    public static int fib(int a){
+        if (a == 0) return 0;
+        else if (a == 1) return 1;
+        else return fib(a - 1) + fib(a - 2);
+    }
+
+    public static void main(String[] args){
+        /*
+        int first = 0;
+        int second = 1;
+        int ans = 0;
+        for (int i = 1; i<=10; i++){
+            ans = first + second;
+            first = second;
+            second = ans;
+        }
+        System.out.println(ans);
+        */
+        System.out.println(fib(10));
+    }
+}
---
+++ codes/recursion.java
@@ -0,0 +1,36 @@
+public class recursion {
+    public static void main(String[] args){
+
+    }
+
+    public static int fib(int n){
+        if(n == 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static int hanoi(int n, char from, char to, char aux){
+        if(n == 1){
+            System.out.println("move disk 1 from " + from + " to " + to);
+            return 1;
+        }
+        int count = hanoi(n - 1, from, aux, to);
+        System.out.println("move disk " + n + " from " + from + " to " + to);
+        count += hanoi(n - 1, aux, to, from);
+        return count;
+    }
+
+    public static int factorial(int n){
+        if(n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static int fib_iter(int n){
+        int a = 0, b = 1, c = 0;
+        for(int i = 1; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
