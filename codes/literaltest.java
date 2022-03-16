82382112162efbd056f271ed3f424a9c365973f0
real-lake050317 <jinojoa317@naver.com>
2022-03-17 01:14:24 +0900

    .'

---
+++ codes/literaltest.java
@@ -0,0 +1,10 @@
+public class literaltest {
+    public static int test(byte a){
+        return a+1;
+    }
+
+    public static void main(String[] args){
+        //System.out.println(test(0)); -> 에러 코드, byte도 int형 리터럴을 사용함. 다운캐스팅 필요.
+        System.out.println(test((byte)0));
+    }
+}
