12be32cc298591fa5d0f951a4db761716b751a6d
real-lake050317 <jinojoa317@naver.com>
2022-03-22 16:08:59 +0900

    .

--- codes/Feb17.java
+++ codes/Feb17.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Feb17{
-    public static void main(String args[]){
+public class Feb17 {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         char ch = '\u0892';
