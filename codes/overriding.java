52b31fbd83701cde993af6db78b17c2d0251d6a3
real-lake050317 <jinojoa317@naver.com>
2022-03-28 01:35:31 +0900

    .

---
+++ codes/overriding.java
@@ -0,0 +1,8 @@
+public class overriding {
+
+    static long add (int a, int b) { return a + b; }
+    static long add (long a, long b) { return a + b; }
+    public static void main(String[] args){
+        System.out.println(add(10, 20));
+    }
+}
