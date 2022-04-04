60b9d550f8af89b6b0a0d7e76168d18578a1f695
real-lake050317 <jinojoa317@naver.com>
2022-04-04 17:30:36 +0900

    .

--- codes/lottomatching.java
+++ codes/lottomatching.java
@@ -39,4 +39,27 @@ public class lottomatching {
         }
         return 0;
     }
+
+    public static int bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr.length - 1; j++){
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static boolean isSameArray(int[] x, int[] y){
+        if (x.length != y.length) return false;
+        
+        for (int i = 0; i < x.length; i++){
+            if (x[i] != y[i]) return false;
+        }
+        
+        return true;
+    }
 }
