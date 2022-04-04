9e94d61a8aa4ae2f9acca873f248407fe2e1b172
real-lake050317 <jinojoa317@naver.com>
2022-04-04 16:59:28 +0900

    .'

---
+++ codes/lottonumgenerator.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class lottonumgenerator {
+    public static void main(String[] args){
+        int[] arr = new int[6];
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++){
+            boolean flg = false;
+            arr[i] = r.nextInt(45);
+            while (true){
+                arr[i] = r.nextInt(45);
+                for (int j = 0; j < i; j++){
+                    if (arr[i] == arr[j]){
+                        flg = true;
+                        break;
+                    }
+                }
+                if (!flg) break;
+            }
+        }
+
+        Arrays.sort(arr); //sort the array
+        
+        //print the array
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
