b0a95b03c281344a83f5db8022db16bc08f8a6aa
real-lake050317 <jinojoa317@naver.com>
2022-04-05 10:13:58 +0900

    .

---
+++ codes/multiDimensionArray.java
@@ -0,0 +1,32 @@
+public class multiDimensionArray {
+    public static void main(String[] args){
+        //2행 3열의 이차원 배열 하나를 선언하고
+        //1행에는 1, 2, 3을, 2행에는 4, 5, 6을 넣어주세요.
+
+        /*
+        //선언, 정의, 원소대입을 순서대로 한 후 출력
+        int[][] arr;
+        arr = new int[2][3];
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+        arr[1][0] = 4;
+        arr[1][1] = 5;
+        arr[1][2] = 6;
+        for (int i = 0; i<2; i++){
+            for (int j = 0; j<3; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        */
+        //선언, 정의, 초기화를 동시에 한 후 출력
+        int[][] arr = new int[][]{{1, 2, 3}, {4, 5, 6}};
+        for (int i = 0; i<2; i++){
+            for (int j = 0; j<3; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
