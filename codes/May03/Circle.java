0ce51f26cf05121ecf42387b75f68d35d429b464
real-lake050317 <jinojoa317@naver.com>
2022-05-09 17:03:04 +0900

    .

--- codes/May03/Circle.java
+++ codes/May03/Circle.java
@@ -4,11 +4,19 @@ public class Circle {
     private double radius;
     final public double pi = Math.PI;
 
+    public Circle(){
+        System.out.println("Hello, World!");
+    }
+
     public void setRadius(double r){
         if (r <= 0){
             throw new IllegalArgumentException("Radius should be even number");
         }
         radius = r;
+    }
+
+    public double getPi() {
+        return pi;
     }
 
     public double areaCircle(){
